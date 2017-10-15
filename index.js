3174766cb3cf909589fe6d5484ffb4f00135bd82
Emmett Agnew <eragnew@gmail.com>
2017-10-14 22:59:40 -0700

    Started working on index.js, updated web3 version to 0.19.0

---
+++ index.js
@@ -0,0 +1,7 @@
+const SolidityCoder = require('web3/lib/solidity/coder.js')
+const Web3 = require('web3')
+
+const fs = require('fs')
+const exchangeABI = require('./exchangeABI')
+
+console.log(exchangeABI)
